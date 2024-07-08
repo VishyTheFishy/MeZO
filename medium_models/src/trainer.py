dbaca6c638e6ae0373ff8a5a3bf0059af0215015
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2024-07-08 02:49:42 -0400

    Update trainer.py

--- medium_models/src/trainer.py
+++ medium_models/src/trainer.py
@@ -452,7 +452,7 @@ class Trainer(LinearHeadTrainer):
         total_train_batch_size = (
             self.args.train_batch_size
             * self.args.gradient_accumulation_steps
-            * (torch.distributed.get_world_size() if self.args.local_rank != -1 else 1)
+            * (1)
         )
         logger.info("***** Running training *****")
         logger.info("  Num examples = %d", self.num_examples(train_dataloader))
